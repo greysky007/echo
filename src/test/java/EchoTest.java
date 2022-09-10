ca192c3c872554a70bcf7dd7dd79635bc6de1c9c
greysky <antonysnork@gmail.com>
2022-09-10 16:07:53 +0300

    first commit

---
+++ src/test/java/EchoTest.java
@@ -0,0 +1,26 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class EchoTest {
+    @Test
+    void shouldPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
+
+        ;
+    }
+}
